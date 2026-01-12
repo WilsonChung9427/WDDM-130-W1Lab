d73f05bfb310c878718e2e8d987ad4c61b075af4
WilsonChung9427 <157180917+WilsonChung9427@users.noreply.github.com>
2026-01-12 17:35:13 -0500

    - Changed the promt from food to movie - Added styling to the webpage

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 startBtn.addEventListener("click", function () {
     var name = prompt("What is your name?");
     var hobby = prompt("What is your favorite hobby?");
-    var food = prompt("What is your favourite food?");
+    var food = prompt("What is your favourite movie?");
 
     if (!name) {
         name = "Ghost";
@@ -11,11 +11,11 @@ startBtn.addEventListener("click", function () {
         hobby = "nothing?!";
     }
     
-    if (!food) {
-        food = "nothing?!"
+    if (!movie) {
+        movie = "nothing?!"
     }
 
-    var msg = `Your name is ${name}, your favorite hobby is ${hobby} and Your favorite food is ${food}`;
+    var msg = `Your name is ${name}, your favorite hobby is ${hobby} and your favorite movie is ${movie}`;
     document.getElementById("outputArea").innerHTML = msg;
 
 });
